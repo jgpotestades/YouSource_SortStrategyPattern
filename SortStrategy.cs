b703e7b3772e181658add255d09d323ea0d71b03
davebanguilan <desbanguilan@gmail.com>
2020-11-23 19:59:28 +0800

    Edit SortStrat

--- SortStrategy.cs
+++ SortStrategy.cs
@@ -32,7 +32,6 @@ namespace YouSource_SortStrategyPattern
                 for (int j = 0; j < n - i - 1; j++)
                     if (arr[j] > arr[j + 1])
                     {
-                        // swap temp and arr[i] 
                         char temp = arr[j];
                         arr[j] = arr[j + 1];
                         arr[j + 1] = temp;
